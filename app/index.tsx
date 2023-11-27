1d14097d2188c51d88d6bb0afde95fd3a6ae0c26
Lucy Zimmerman <30590230+lucyzimmo@users.noreply.github.com>
2023-11-26 18:07:56 -0800

    textbox for phone component
    need to implement back-end and comprehensive input-masking at a later point

--- app/index.tsx
+++ app/index.tsx
@@ -24,7 +24,6 @@ export default function Home() {
           </View>
         }
       />
-      <PhoneNumberInput />
     </View>
   );
 }
--- components/textBoxPhone.tsx
+++ components/textBoxPhone.tsx
@@ -3,19 +3,51 @@ import { TextInput, View, Image, StyleSheet, Text } from "react-native";
 import Colors from "../constants/Colors";
 import shadow from "../constants/shadows";
 
-interface PhoneNumberInputProps {}
+interface PhoneNumberInputProps {
+  onChange?: (text: string) => void;
+}
 
-const PhoneNumberInput: React.FC<PhoneNumberInputProps> = ({}) => {
+const PhoneNumberInput: React.FC<PhoneNumberInputProps> = (
+  props: PhoneNumberInputProps
+) => {
   const [inputText, setInputText] = useState("");
-
+  //check that number is within format
+  const validateNumber = (text: string) => {
+    if (text.length < 11) {
+      setInputText(text);
+    }
+    if (text.length == 10) {
+      setInputText("+1 " + text);
+    }
+  };
+  {
+    /*IMPLEMENT MASKING LATER 
+    const formatNumber = (text: string) => {
+    let maskedInput = "";
+    if (text.length >= 1) {
+      maskedInput += `(${text.slice(0, 3)}`;
+    }
+    if (text.length > 3) {
+      maskedInput += `) ${text.slice(3, 6)}`;
+    }
+    if (text.length > 6) {
+      maskedInput += `-${text.slice(6, 10)}`;
+    }
+    
+  }; */
+  }
   return (
     <View style={styles.container}>
       <View style={styles.inputBox}>
+        <Image
+          source={require("../assets/images/americanflag.png")}
+          style={styles.image}
+        />
         <TextInput
           style={styles.input}
           placeholder="Enter your phone number"
           value={inputText}
-          onChangeText={(text) => setInputText(text)}
+          onChangeText={(text) => validateNumber(text)}
         />
       </View>
     </View>
@@ -28,17 +60,23 @@ const styles = StyleSheet.create({
     paddingVertical: 20,
     paddingHorizontal: 12,
     marginVertical: 10,
+    marginHorizontal: 20,
     // Full width
-    width: "100%",
     alignItems: "center",
     justifyContent: "center",
     // Shadow
     ...shadow,
   },
+  image: {
+    marginRight: 10,
+    maxHeight: 25,
+    maxWidth: 40,
+    aspectRatio: 1,
+  },
   inputBox: {
     flexDirection: "row",
     alignItems: "center",
-    marginHorizontal: "15%",
+    marginHorizontal: 10,
   },
   input: {
     color: Colors.black,

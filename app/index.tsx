0fa5adda3cdb6321c19e22e172cbf91a90e4aa32
Lucy Zimmerman <30590230+lucyzimmo@users.noreply.github.com>
2023-11-27 22:07:15 -0800

    dropdown menu

--- app/index.tsx
+++ app/index.tsx
@@ -4,28 +4,34 @@ import Button from "../components/button";
 import TextButton from "../components/textButton";
 import { router } from "expo-router";
 import React from "react";
-
+import Dropdown from "../components/dropdown";
 
 export default function Home() {
-    return (
-        <View>
-            <Header
-                showLogo
-                title="Welcome,"
-                subtitle={"Get Ready to say \"F*** it\" to your addictions"}
-                body={"However, to help you I need to know a bit about you.\n\nReady?"}
-                action={
-                    // Buttons go here
-                    <View style={{ width: "100%" }}>
-                        <Button title="F*** Yeah!" onPress={() => {
-                            router.push("/1age");
-                        }} />
-                        <TextButton title="I Already Have An Account" onPress={() => {
-                            router.push("/7phone");
-                        }} />
-                    </View>
-                }
+  return (
+    <View>
+      <Header
+        showLogo
+        title="Welcome,"
+        subtitle={'Get Ready to say "F*** it" to your addictions'}
+        body={"However, to help you I need to know a bit about you.\n\nReady?"}
+        action={
+          // Buttons go here
+          <View style={{ width: "100%" }}>
+            <Button
+              title="F*** Yeah!"
+              onPress={() => {
+                router.push("/1age");
+              }}
             />
-        </View>
-    );
+            <TextButton
+              title="I Already Have An Account"
+              onPress={() => {
+                router.push("/7phone");
+              }}
+            />
+          </View>
+        }
+      />
+    </View>
+  );
 }

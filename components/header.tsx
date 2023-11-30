d972e0a71f2582579743fb97810affe60ee4fead
Eduardo M <contact@eduardom.dev>
2023-11-29 20:19:26 -0800

    Add back button

--- components/header.tsx
+++ components/header.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { View, SafeAreaView, Text, StyleSheet } from "react-native";
+import { View, SafeAreaView, Text, StyleSheet, TouchableOpacity } from "react-native";
 import Svg, { Path } from "react-native-svg";
 import Colors from "../constants/Colors";
 import Logo from "./logo";
 import textStyle from "../constants/textStyles";
 import ProgressBar from "./progressBar";
+import { NavArrowLeft } from "iconoir-react-native";
 
 interface HeaderProps {
   color?: keyof typeof Colors;
@@ -29,30 +30,43 @@ const Header = (props: HeaderProps = {}) => {
           paddingHorizontal: 34,
         }}
       >
+        {props.onBack ? (
+          <TouchableOpacity onPress={props.onBack}>
+            <NavArrowLeft width={24} height={24} strokeWidth={4} color={Colors.white} style={{
+              paddingHorizontal: 40,
+              marginTop: 46,
+              marginBottom: 10,
+            }} />
+          </TouchableOpacity>
+        ) : <View style={{
+          paddingHorizontal: 34,
+          marginTop: 80,
+
+        }} />}
         {/*Row that contains the logo and the progress bar*/}
         <View
           style={{
             flexDirection: "row",
-            alignItems: "flex-start",
-            justifyContent: "space-between",
+            alignItems: "center",
             paddingHorizontal: 34,
+            marginTop: 20,
+            marginBottom: 20,
           }}
         >
+          {/*If onCancel is not null, show the cancel button*/}
           {/*If  showLogo is true, show the logo*/}
           {props.showLogo && (
             <Logo
               style={{
                 height: 36,
                 width: 51,
-                marginTop: 80,
-                marginBottom: 20,
               }}
               color={Colors.white}
             />
           )}
           {/*If showProgress is true, show the progress bar*/}
           {props.showProgress && props.currentStep && props.totalSteps && (
-            <ProgressBar fillPercentage={(props.currentStep / props.totalSteps) * 100} style={{ marginTop: 90, marginLeft: 30 }} />
+            <ProgressBar fillPercentage={(props.currentStep / props.totalSteps) * 100} style={{ marginLeft: 30 }} />
           )}
         </View>
         {/*Column that contains the title and subtitle*/}

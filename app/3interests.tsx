490dfa1cf510172d4cb011516fa4da9b824e14e6
Eduardo M <contact@eduardom.dev>
2023-11-29 21:52:54 -0800

    Update 3interests.tsx

--- app/3interests.tsx
+++ app/3interests.tsx
@@ -3,8 +3,15 @@ import Header from "../components/header";
 import { router } from "expo-router";
 import React from "react";
 import TextBoxInput from "../components/textBox";
+import Checkbox from "../components/checkbox";
+import Dropdown from "../components/dropdown";
+import FabButton from "../components/fab";
+import { ArrowRight } from "iconoir-react-native";
+import textStyle from "../constants/textStyles";
 
 export default function Home() {
+
+
 
     return (
         <View>
@@ -19,6 +26,17 @@ export default function Home() {
                     // Buttons go here
                     <View style={{ width: "100%" }}>
                         <TextBoxInput placeholder="Type to Search" keyboardType="default" />
+                        <Dropdown options={["Soccer", "Listening to Music", "Animals", "Running", "Football", "Tennis", "Biking"]} onMultiselect={(_) => { }} onPress={() => { }} />
+                        <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+                            <Text style={textStyle.caption}>
+                                Select at least 2 interests
+                            </Text>
+                            <FabButton onPress={() => {
+                                router.push("/4spotify");
+                            }}>
+                                <ArrowRight color="black" width={30} height={30} strokeWidth={2} />
+                            </FabButton>
+                        </View>
                     </View>
                 }
                 showProgress

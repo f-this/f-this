7d0f56a7fb0fd6952d0458d4e74b07dbe6007411
fbottazzini <fbottazzini@cs.stanford.edu>
2023-11-30 22:20:54 -0800

    Customize page
    Customize page

--- app/index.tsx
+++ app/index.tsx
@@ -27,7 +27,8 @@ export default function Home() {
             <TextButton
               title="I Already Have An Account"
               onPress={() => {
-                router.push("/7phone");
+                //router.push("/7phone");
+                router.push("/customize");
               }}
             />
           </View>

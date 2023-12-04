eea242fb0f50a8cbe5f1eeb45d0952bf012efb2a
Eduardo M <contact@eduardom.dev>
2023-12-03 19:42:21 -0800

    Update index.tsx

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,14 +1,25 @@
 import { View, Text } from "react-native";
 import { StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Header from "../../components/header";
 
 
 export default function Home() {
 
     return (
         <View>
-            <View style={styles.container}>
-                <Text>Home</Text>
-            </View>
+            <Header
+                showLogo
+                title="Welcome,"
+                subtitle={'Get Ready to say "F*** it" to your addictions'}
+                body={"However, to help you I need to know a bit about you.\n\nReady?"}
+                action={
+                    // Buttons go here
+                    <View style={{ width: "100%" }}>
+                        
+                    </View>
+                }
+            />
         </View>
     );
 }
@@ -18,6 +29,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         height: 100,
+        width: 100,
         backgroundColor: "red",
     },
 });

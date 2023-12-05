663918fd15b20d74cfb5f21b9aca8b6b63c8cc8e
Eduardo M <contact@eduardom.dev>
2023-12-05 15:14:15 -0800

    BlurView appbar

--- app/(app)/(with-appbar)/_layout.tsx
+++ app/(app)/(with-appbar)/_layout.tsx
@@ -2,13 +2,10 @@ import { Redirect, Stack } from 'expo-router';
 import { Text, View } from 'react-native';
 import React from 'react';
 import AppBar from '../../../components/appBar';
-import { BlurView } from 'expo-blur';
 
 export default function AppLayout() {
     return <View style={{ flex: 1, flexDirection: "column" }}>
         <Stack screenOptions={{ header: () => null }} />
-        <BlurView intensity={100} >
-            <AppBar />
-        </BlurView>
+        <AppBar />
     </View>;
 }

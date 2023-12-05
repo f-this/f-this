14ed1bda346f53a36dfae8cddf7e6c2b34703a8d
Eduardo M <contact@eduardom.dev>
2023-12-04 16:48:50 -0800

    Add animtion to checkup buttons

--- components/dashboard/daily-checkup.tsx
+++ components/dashboard/daily-checkup.tsx
@@ -2,11 +2,13 @@ import { View, Text, StyleSheet } from "react-native";
 import shadow from "../../constants/shadows";
 import textStyle from "../../constants/textStyles";
 import Colors from "../../constants/Colors";
+import { Gesture, GestureDetector } from "react-native-gesture-handler";
+import Animated, { useSharedValue, useAnimatedStyle, withTiming } from "react-native-reanimated";
 
 export default function Checkup() {
   return (
     <View style={styles.container}>
-      <View style={styles.innerContainer}>
+      <View style={styles.innerContainer as any}>
         <Text style={styles.title}>Daily Check-Up</Text>
         <Text style={styles.body}>
           Research shows that after 14 days without nicotine you will start
@@ -14,15 +16,48 @@ export default function Checkup() {
         </Text>
 
         <View style={styles.buttonContainer}>
-          <View style={styles.button}>
-            <Text style={styles.buttonText}>Fake News</Text>
-          </View>
-          <View style={styles.blueButton}>
-            <Text style={styles.blueButtonText}>OMG so real</Text>
-          </View>
+          <SlimButton title="Fake News" onPress={() => { }} white />
+          <SlimButton title="OMG so real" onPress={() => { }} white={false} />
         </View>
       </View>
     </View>
+  );
+}
+
+
+function SlimButton(props: { title: string, onPress: () => void, white: boolean }) {
+  const shadowOffsetWidth = useSharedValue(3);
+  const shadowOffsetHeight = useSharedValue(4);
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      shadowOffset: {
+        width: shadowOffsetWidth.value,
+        height: shadowOffsetHeight.value,
+      },
+    };
+  });
+
+  const tap = Gesture.Tap()
+    .onBegin(() => {
+      shadowOffsetWidth.value = withTiming(0, { duration: 50 });
+      shadowOffsetHeight.value = withTiming(0, { duration: 50 });
+    })
+    .onEnd(() => {
+      props.onPress();
+    })
+    .onFinalize(() => {
+      shadowOffsetWidth.value = withTiming(3);
+      shadowOffsetHeight.value = withTiming(4);
+    })
+    .runOnJS(true);
+
+  return (
+    <GestureDetector gesture={tap}>
+      <Animated.View style={[props.white ? styles.button : styles.blueButton as any, animatedStyle]}>
+        <Text style={props.white ? styles.buttonText : styles.blueButtonText}>{props.title}</Text>
+      </Animated.View>
+    </GestureDetector>
   );
 }
 
